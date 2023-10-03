53dace7d1c3e34fe6f326ea76defd3bee9dbcf77
Jose Alfredo Vazquez Jimenez <fredy@MacBook-Air-de-Jose.local>
2023-10-02 23:36:23 -0600

    Algunas formas de como recuperar las cookies en nextjs

--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppBar, IconButton, Link, Toolbar, Typography } from "@mui/material";
-import { MenuBookOutlined } from "@mui/icons-material";
+import { MenuOutlined } from "@mui/icons-material";
 
 import NextLink from "next/link";
 
@@ -9,7 +9,7 @@ export const Navbar = () => {
     <AppBar position="sticky" elevation={0}>
       <Toolbar>
         <IconButton size="large" edge="start">
-          <MenuBookOutlined />
+          <MenuOutlined />
         </IconButton>
 
         <NextLink href="/" passHref>
--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,6 +1,7 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState, useEffect, FC } from "react";
 import { Layout } from "../components/layout";
 import {
+  Button,
   Card,
   CardContent,
   FormControl,
@@ -9,20 +10,33 @@ import {
   Radio,
   RadioGroup,
 } from "@mui/material";
+import axios from "axios";
 import Cookies from "js-cookie";
 
-export const ThemeChangerPage = () => {
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+import { GetServerSideProps } from "next";
+
+const ThemeChangerPage: FC = (props) => {
   const [currentTheme, setCurrentState] = useState("light");
 
   const handleChangeTheme = (event: ChangeEvent<HTMLInputElement>) => {
     const selectTheme = event.target.value;
 
-    console.log({ selectTheme });
-
     setCurrentState(selectTheme);
 
     Cookies.set("theme", selectTheme);
   };
+
+  const onClick = async () => {
+    const resp = await axios.get("/api/hello");
+
+    console.log(resp);
+  };
+
+  useEffect(() => {
+    console.log("Coockies: ", Cookies.get("theme"));
+  }, []);
 
   return (
     <Layout>
@@ -44,10 +58,26 @@ export const ThemeChangerPage = () => {
               />
             </RadioGroup>
           </FormControl>
+
+          <Button onClick={onClick}>Solicitud</Button>
         </CardContent>
       </Card>
     </Layout>
   );
+};
+
+/**
+ * Con esta importancion "getServerSideProps" nosotros le decios a Next
+ * que generamos este page del lado del servidor mediante SSR "SERVER SIDE RENDERING"
+ **/
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { theme = "light", name = "No name" } = req.cookies;
+  return {
+    props: {
+      theme,
+      name,
+    },
+  };
 };
 
 export default ThemeChangerPage;
